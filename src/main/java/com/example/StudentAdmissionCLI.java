26c372877f09a9a25421abe926940800d2d149f9
ArpitaV24042003 <1ep22cs012.arpitav@gmail.com>
2025-04-03 19:55:24 +0530

    Fixed a smallissue in code

--- src/main/java/com/example/StudentAdmissionCLI.java
+++ src/main/java/com/example/StudentAdmissionCLI.java
@@ -1,4 +1,4 @@
-package src.main.java.com.example;
+package com.example;
 import java.io.*;
 import java.util.*;
 
@@ -33,6 +33,11 @@ public class StudentAdmissionCLI {
             System.out.println("3. Search Student by ID");
             System.out.println("4. Exit");
             System.out.print("Enter your choice: ");
+              if (!scanner.hasNextInt()) {  // Prevents NoSuchElementException
+                    System.out.println("Invalid input! Please enter a number.");
+                    scanner.next();  // Consume the invalid input
+                    continue;
+                }
             int choice = scanner.nextInt();
             scanner.nextLine();
             
